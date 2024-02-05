63aca2df67d4108d5a622ead885917acbe38a228
raissaputi <raissa.puti@gmail.com>
2024-02-05 23:19:29 +0700

    add productId

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product){
+        String productId = String.valueOf(UUID.randomUUID());
+        product.setProductId(productId);
         productData.add(product);
         return product;
     }
