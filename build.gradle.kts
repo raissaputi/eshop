96a1b3c7271ceca39eab1c6ac5c8c70193484e17
raissaputi <raissa.puti@gmail.com>
2024-02-12 20:57:51 +0700

    setup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -49,6 +50,7 @@ tasks.register<Test>("unitTest") {
 		excludeTestsMatching("*FunctionalTest")
 	}
 }
+
 tasks.register<Test>("functionalTest") {
 	description = "Runs functional tests."
 	group="verification"
@@ -60,4 +62,16 @@ tasks.register<Test>("functionalTest") {
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
+
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
 }
