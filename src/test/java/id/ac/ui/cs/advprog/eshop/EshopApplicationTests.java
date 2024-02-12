07c6c9dc0fe017aaab80c7e13ee90a2a13b26f11
raissaputi <raissa.puti@gmail.com>
2024-02-13 00:27:10 +0700

    add main app test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,11 +3,14 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class EshopApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void main() {}
 
 }
