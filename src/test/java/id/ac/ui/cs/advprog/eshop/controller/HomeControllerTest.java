5a435b54739207fe72f16598e2a18126d0339bc3
raissaputi <raissa.puti@gmail.com>
2024-02-13 04:21:56 +0700

    fix homecontrollertest

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -1,22 +1,15 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
-    void testHomePage() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("homePage"));
+    void testHomePage() {
+        HomeController homeController = new HomeController();
+        String viewName = homeController.homePage();
+
+        assertEquals("homePage", viewName);
     }
 }
