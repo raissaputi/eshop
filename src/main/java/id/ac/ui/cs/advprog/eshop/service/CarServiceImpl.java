d858cfea021bbe0f67af0e7418875cf37fec5efc
raissaputi <raissa.puti@gmail.com>
2024-02-23 19:44:32 +0700

    remove comment

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -14,7 +14,6 @@ public class CarServiceImpl implements CarService{
 
     @Override
     public Car create(Car car){
-        // TODO
         carRepository.create(car);
         return car;
     }
@@ -35,13 +34,11 @@ public class CarServiceImpl implements CarService{
 
     @Override
     public void update (String carId, Car car){
-        // TODO
         carRepository.update(carId, car);
     }
 
     @Override
     public void deleteCarById(String carId) {
-        // TODO
         carRepository.delete(carId);
     }
 }
