55446fb1a8600da5b3fd0b8735bc21474af9f9c1
raissaputi <raissa.puti@gmail.com>
2024-02-23 22:20:48 +0700

    fix car controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/car")
-class CarController extends ProductController {
+class CarController {
     @Autowired
     private CarService carservice;
 
